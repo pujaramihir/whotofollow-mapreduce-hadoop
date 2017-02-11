06e35ce3bf3697f305b5f690fefcf736756e35ad
mohit <mohit@mohit-Inspiron-5537>
2017-02-11 17:54:50 -0500

    Second Mapper and Reducer created

--- src/whotofollow.java
+++ src/whotofollow.java
@@ -1,4 +1,8 @@
+import java.awt.Point;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.StringTokenizer;
 
@@ -9,12 +13,9 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Mapper.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
-/**
- * 
- */
 
 /**
  * @author Mihir Pujara & Mohit Pujara
@@ -28,37 +29,37 @@ public class whotofollow {
 	 *
 	 */
 	public class MapperFirst extends Mapper<Object, Text, IntWritable, IntWritable> {
-		
+
 		public void map(Object key, Text values, Context context) throws IOException, InterruptedException {
-			
-			// Key is ignored as.i""t only stores the offset of the line in the text file
-            StringTokenizer st = new StringTokenizer(values.toString());
-            
-            // user and follow will be the elements in the emitted pairs.
-            IntWritable user = new IntWritable();
-            IntWritable follow = new IntWritable();
-            
-            int temp = Integer.parseInt(st.nextToken());
-            user.set(temp); // set user value
-            
-            
-            //get follower user value from text
-        	while (st.hasMoreTokens()) {
-        		
-        		//convert followers value to int and set it into follow intwritable
-            	temp = Integer.parseInt(st.nextToken());
-                follow.set(temp);
-                
-                // emit user and follow key value pair 
-                context.write(follow, user);
-                
-                // emit negatibe user and follow key value pair
-                context.write(new IntWritable(-user.get()), new IntWritable(-follow.get()));
-            }
+
+			// Key is ignored as.i""t only stores the offset of the line in the
+			// text file
+			StringTokenizer st = new StringTokenizer(values.toString());
+
+			// user and follow will be the elements in the emitted pairs.
+			IntWritable user = new IntWritable();
+			IntWritable follow = new IntWritable();
+
+			int temp = Integer.parseInt(st.nextToken());
+			user.set(temp); // set user value
+
+			// get follower user value from text
+			while (st.hasMoreTokens()) {
+
+				// convert followers value to int and set it into follow
+				// intwritable
+				temp = Integer.parseInt(st.nextToken());
+				follow.set(temp);
+
+				// emit user and follow key value pair
+				context.write(follow, user);
+
+				// emit negatibe user and follow key value pair
+				context.write(new IntWritable(-user.get()), new IntWritable(-follow.get()));
+			}
 		}
 	}
-	
-	
+
 	/**
 	 * Class for first reduce task
 	 * 
@@ -66,58 +67,129 @@ public class whotofollow {
 	 *
 	 */
 	public class ReducerFirst extends Reducer<IntWritable, IntWritable, IntWritable, Text> {
-		public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-			
+		public void reduce(IntWritable key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+
 			IntWritable user = key;
-			
+
 			StringBuffer stringBuffer = new StringBuffer("");
-			
+
 			// generate iterator from iterable
-			Iterator<IntWritable> intValues = values.iterator(); 
-			
-			// take all elements one by one from iterator and store it to string buffer
+			Iterator<IntWritable> intValues = values.iterator();
+
+			// take all elements one by one from iterator and store it to string
+			// buffer
 			while (intValues.hasNext()) {
-            	
-                int whofollowsuser = intValues.next().get();
-                stringBuffer.append(whofollowsuser+ " ");
-                
-            }
-			
+
+				int whofollowsuser = intValues.next().get();
+				stringBuffer.append(whofollowsuser + " ");
+
+			}
+
 			// emit user and result
 			Text result = new Text(stringBuffer.toString());
-            context.write(user, result);
+			context.write(user, result);
 		}
 	}
-	
-	
-	public static void main(String[] args) {
 
-		// create configuration 
+	/**
+	 * Second Mapper Class to find the Similarity
+	 * 
+	 * @author Mohit Pujara
+	 *
+	 */
+	public static class MapperSecond extends Mapper<Object, Text, IntWritable, IntWritable> {
+
+		public void map(Object key, Text values, Context context) throws IOException, InterruptedException {
+
+			// String Tockenizer used to separate values from text file
+			StringTokenizer st1 = new StringTokenizer(values.toString());
+			StringTokenizer st2 = new StringTokenizer(values.toString());
+
+			IntWritable follower1 = new IntWritable();
+			IntWritable follower2 = new IntWritable();
+
+			int temp;
+
+			int first = Integer.parseInt(st1.nextToken());
+			st2.nextToken();
+
+			if (first < 0) {
+
+				// Emits Negative values from key value pairs
+				while (st1.hasMoreTokens()) {
+
+					follower1.set(Math.abs(first));
+					follower2.set(Integer.parseInt(st1.nextToken()));
+
+					context.write(follower1, follower2);
+				}
+				return;
+			}
+		}
+	}
+
+	public static class ReducerSecond extends Reducer<IntWritable, IntWritable, IntWritable, Text> {
+
+		public class Followers {
+			public int positive = 0;
+			public int negative = 0;
+			public int count = 0;
+		}
+
+		// The reduce method
+		public void reduce(IntWritable key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+
+			IntWritable user = key;
+			HashMap<Integer, Followers> data = new HashMap<Integer, Followers>();
+			StringBuffer stringBuffer = new StringBuffer("");
+			Iterator<IntWritable> i = values.iterator();
+			Iterator<Integer> s = data.keySet().iterator();
+			ArrayList<Followers> finalData = new ArrayList<Followers>();
+			Iterator<Followers> d = finalData.iterator();
+
+		}
+	}
+
+	public static void main(String[] args)
+			throws ClassNotFoundException, IllegalStateException, IllegalArgumentException, InterruptedException {
+
+		// create configuration
 		Configuration conf = new Configuration();
-		
+
 		// create job for first map reduce task
-        Job firstJob;
-		try 
-		{
+		Job firstJob;
+		try {
+
 			firstJob = Job.getInstance(conf, "who to follow first job");
-	        firstJob.setJarByClass(whotofollow.class);
-	        
-	        // set map class reference
-	        firstJob.setMapperClass(MapperFirst.class);
-	        
-	        // set reduce class reference 
-	        firstJob.setReducerClass(ReducerFirst.class);
-	        
-	        // set output class 
-	        firstJob.setOutputKeyClass(IntWritable.class);
-	        
-	        // set output value class
-	        firstJob.setOutputValueClass(IntWritable.class);
-	        
-	        // set input and output details
-	        FileInputFormat.addInputPath(firstJob, new Path(args[0]));
-	        FileOutputFormat.setOutputPath(firstJob, new Path(args[1]));
-	        
+			firstJob.setJarByClass(whotofollow.class);
+			firstJob.setMapperClass(MapperFirst.class);
+			firstJob.setReducerClass(ReducerFirst.class);
+			firstJob.setOutputKeyClass(IntWritable.class);
+			firstJob.setOutputValueClass(IntWritable.class);
+
+			FileInputFormat.addInputPath(firstJob, new Path(args[0]));
+			FileOutputFormat.setOutputPath(firstJob, new Path(args[1]));
+
+			// Check whether first job is completed or not
+			if (firstJob.waitForCompletion(true)) {
+
+				// second job
+				Configuration conf2 = new Configuration();
+				Job secondJob = Job.getInstance(conf2, "who to follow second job");
+				secondJob.setJarByClass(whotofollow.class);
+				secondJob.setMapperClass(MapperSecond.class);
+				secondJob.setReducerClass(ReducerSecond.class);
+				secondJob.setOutputKeyClass(IntWritable.class);
+				secondJob.setOutputValueClass(IntWritable.class);
+
+				FileInputFormat.addInputPath(secondJob, new Path(args[1]));
+				FileOutputFormat.setOutputPath(secondJob, new Path(args[2]));
+
+				System.exit(secondJob.waitForCompletion(true) ? 0 : 1);
+			}
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
